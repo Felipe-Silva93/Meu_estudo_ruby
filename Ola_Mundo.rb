821ecf692a5bfe9b67861219178f213e4628041c
felipe <felipesilva4127@gmail.com>
2022-10-21 09:09:49 -0300

    Alteracao02

--- Ola_Mundo.rb
+++ Ola_Mundo.rb
@@ -1,1 +1,19 @@
+
+#Primeiros passo em Ruby
 puts"Ola Mundo"
+
+nome ="Felipe"
+
+menssagem = "Bem vindo #{nome}"
+
+menssagem2 =<<~MSG
+ Olá #{nome}
+
+ bem vindo!
+
+MSG
+
+menssagem3 = %Q(bem vindo meu caro amigo #{nome})
+puts menssagem3
+
+puts "o tamanho da menssagem3 é: #{menssagem3.length()}"
