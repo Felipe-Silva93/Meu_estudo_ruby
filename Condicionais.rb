dca03068ede2ff82f84fcab8d89ac0c563456219
felipe <felipesilva4127@gmail.com>
2022-11-03 19:49:13 -0300

    Condicionais

---
+++ Condicionais.rb
@@ -0,0 +1,14 @@
+valor = gets.chomp.to_i
+
+#p valor.class
+
+#p valor
+if valor > 20
+    puts "valor maior que 20"
+
+elsif valor == 20
+    puts" Esse valor é igual a 20"   
+  
+else
+    puts "Esse valor é menor que 20"    
+end    
