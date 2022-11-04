220dbff750dc9498b9f47739a1ddb76ff4b80ac2
felipe <felipesilva4127@gmail.com>
2022-11-04 14:20:28 -0300

    lacos-02

--- lacos.rb
+++ lacos.rb
@@ -1,6 +1,6 @@
 
 
-valor = 0
+#valor = 0
 
 #while(valor <10)
 #   puts " o valor é #{valor}"
@@ -15,6 +15,17 @@ valor = 0
   #  valor += 1
 #end
 
-for meu_valor in [0,1,2,3,4,5]
-    puts"meu valor é #{meu_valor}"
-end
+#for meu_valor in [0,1,2,3,4,5]
+ #   puts"meu valor é #{meu_valor}"
+#end
+ 
+#for meu_valor in 0..5
+ #   puts"meu valor é #{meu_valor}"
+#end
+
+lista = [0,1,2,3,4,5]
+
+lista.each do |meu_valor|
+    puts"Meu valor |usando each| é #{meu_valor}"
+end    
+
