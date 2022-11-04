1e33f5e73a55b26e7fe57dfaa216878cbdf9d844
felipe <felipesilva4127@gmail.com>
2022-11-04 13:52:33 -0300

    lacos-01

---
+++ lacos.rb
@@ -0,0 +1,20 @@
+
+
+valor = 0
+
+#while(valor <10)
+#   puts " o valor é #{valor}"
+#    valor += 1
+#end   
+
+#while(valor <10)
+#    puts " o valor é #{valor}"
+
+
+ #   break if valor == 5
+  #  valor += 1
+#end
+
+for meu_valor in [0,1,2,3,4,5]
+    puts"meu valor é #{meu_valor}"
+end
