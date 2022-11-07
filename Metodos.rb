2b4d4b01dc513bf22eed75e403726880641c5698
felipe <felipesilva4127@gmail.com>
2022-11-07 11:40:34 -0300

    metodo-02

--- Metodos.rb
+++ Metodos.rb
@@ -3,10 +3,23 @@
 def soma (valor1,valor2)
     puts "Estou somando #{valor1} + #{valor2}"
 
-    return valor1 + valor2
+     valor1 + valor2
 
 end    
 
-puts" fora do metodo estou chamando o return e imprimindo na tela"
+puts" fora do metodo estou chamando o return que é opcional e imprimindo na tela"
 
 puts soma(10,10)   
+
+puts "-----passando um valor como parametro opcional-----"
+
+def soma2 (valor11,valor=0)
+    puts "Estou somando #{valor11} + #{valor}"
+
+     valor11 + valor
+
+end    
+
+puts" imprimindo na tela"
+
+puts soma2(10)   
