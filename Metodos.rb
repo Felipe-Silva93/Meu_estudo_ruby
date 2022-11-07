f2f43d740c94eb132e91dd43f894993bd4487026
felipe <felipesilva4127@gmail.com>
2022-11-07 11:34:29 -0300

    Metodos-01

---
+++ Metodos.rb
@@ -0,0 +1,12 @@
+
+
+def soma (valor1,valor2)
+    puts "Estou somando #{valor1} + #{valor2}"
+
+    return valor1 + valor2
+
+end    
+
+puts" fora do metodo estou chamando o return e imprimindo na tela"
+
+puts soma(10,10)   
