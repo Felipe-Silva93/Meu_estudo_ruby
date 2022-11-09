085a9df8ea521758e1132d2a82164121363764d9
felipe <felipesilva4127@gmail.com>
2022-11-09 10:50:10 -0300

    exercicio-1.1

--- ExercicioFinal.rb
+++ ExercicioFinal.rb
@@ -1,25 +1,5 @@
 def palindromo?(palavra)# para um return tipo boolean sempre usa o "?"
-    palavra = palavra.downcase
-    palavra_revers =""
-
-    #percorrer de trás para frente
-
-    indice = palavra.length
-
-    until indice == 0 #until "faz enquanto" não atinge a posição
-        letra = palavra[indice -1]
-        palavra_revers << letra 
-        indice -= 1
-
-    end
-
-     palavra_revers == palavra ? true : false 
-
-     #if palavra_revers == palavra
-      #  return true
-    #else
-      #  return false
-    #end
+   palavra.downcase == palavra.downcase.reverse
 
 end    
 
